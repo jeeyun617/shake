33ccc82d1b8d9166ac164c612784c7b383e39863
jeeyun <jeeyun617@naver.com>
2015-05-31 12:51:58 +0900

    이미지 업로드 구현
    1. 이미지 필드 추가
    2. settings에서 MEDIA_URL, MEDIA_ROOT 추가
    3. urls에서 mapping
    4. template

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    photo = models.ImageField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- shake_practice/urls.py
+++ shake_practice/urls.py
@@ -1,20 +1,4 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-"""shake_practice URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -22,3 +6,12 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog')),
 ]
+
+if settings.DEBUG and settings.MEDIA_URL.startswith('/'):
+    media_url = settings.MEDIA_URL.strip('/')               # '/media/' -> 'media'
+    media_url_pattern = '^' + media_url + '/(?P<path>.*)$'  # '^media/(?P<path>.*)$'
+    urlpatterns += [
+        url(media_url_pattern, 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
